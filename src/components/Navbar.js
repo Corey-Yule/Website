bc274f249762a5a3ab90edcceb10154f55ad5ab7
Corey-Yule <coreyyule22@gmail.com>
2025-02-17 19:17:51 +0000

    Fixed navbar error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <>
-        <nav className="navbar">
-            <div className="navbar-container">
-                <link to="/" className="navbar-logo">
-                Corey <i className="fab fa-typo3"/>
-                </link>
-            </div>
-        </nav>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            Corey <i className="fab fa-typo3" />
+          </Link>
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar;
